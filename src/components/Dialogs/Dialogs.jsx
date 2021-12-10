4354b91e13aa898392931fac6a88bbf5f6109881
Hieorhiu Yurii <j.hieorhiu@autodoc.eu>
2021-12-10 18:41:36 +0200

    18

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,11 @@
+import s from "../Dialogs/Dialogs.module.css";
+
+function Dialogs() {
+  return (
+    <div>
+      Dialogs
+    </div>
+  );
+}
+
+export default Dialogs;
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -6,7 +6,7 @@ function Post(props) {
       <img src="https://static.1tv.ru/uploads/photo/image/2/huge/4062_huge_876c41f50e.jpg" alt=""/>
       { props.message }
       <div>
-        <span>{ props.likes }</span>
+        <span>like</span> { props.likesCount }
       </div>
     </div>
   );
